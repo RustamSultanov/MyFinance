9ed1893161fea18975da29a6e315576864fc121c
Rustam <rustamelutingol@gmail.com>
2016-10-31 22:14:42 +0300

    дз

--- finance/views.py
+++ finance/views.py
@@ -1,53 +1,41 @@
 from django.shortcuts import render
-
+from datetime import date
+from decimal import Decimal
+from .models import Account, Charge
+from random import randint
+from .models import Account
 # Create your views here.
-from django.http import HttpResponse
 
 
-def front_page(request):
-	return HttpResponse('<html>'
-                        '<head>'
-                        '<title>Главная страница</title>'
-                        '</head>'
-                        '<body>'
-                        '<h1>Приветствие</h1><br>'
-                        '<a href="charges">Выписка по счету</a>'
-                        '<body/>'
-                        '</html>')
+def random_transactions( ):
+    today = date.today( )
+    start_date = today.replace(month=1, day=1).toordinal()
+    end_date = today.toordinal()
+    while True:
+        start_date = randint(start_date, end_date)
+        random_date = date.fromordinal(start_date)
+        if random_date >= today:
+            break
+        random_value = randint(-10000, 10000), randint(0, 99)
+        random_value = Decimal('%d.%d' % random_value)
+        yield random_date, random_value
 
-def charges_page(request):
-	return HttpResponse('<html>'
-                        '<head>'
-                        '<title>Выписка по счету</title>'
-                        '</head>'
-                        '<body>'
-                        '<h1>Выписка по счету</h1><br>'
-                        '<table border=3>'
-                        '<tr>'
-                        '<td>'
-                        'Один'
-                        '</td>'
-                        '<td>'
-                        'Четыре'
-                        '</td>'
-                        '</tr>'
-                        '<tr>'
-                        '<td>'
-                        'Два'
-                        '</td>'
-                        '<td>'
-                        'Пять'
-                        '</td>'
-                        '</tr>'
-                        '<tr>'
-                        '<td>'
-                        'Три'
-                        '</td>'
-                        '<td>'
-                        'Шесть'
-                        '</td>'
-                        '</tr>'
-                        '</table>'
-                        '<a href="/">Главная страница</a>'
-                        '<body/>'
-                        '</html>')
+
+def main(request):
+    return render(request, 'finance/charges_page.html')
+
+
+def charges(request):
+    charges = []
+    positive = 0
+    negative = 0
+    for i in range(25):
+        Date, Value = random_transactions().__next__()
+        if Value > 0 : positive+= Value
+        else : negative+= Value
+        charge = Charge.create(Value, Date)
+        charges.append(charge)
+
+    return render(request,
+                  'finance/charges_view.html',
+                  {"charges": charges, "positive" : positive, "negative" : negative})
